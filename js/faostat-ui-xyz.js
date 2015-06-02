f9ebd444d2003dda42ddc4502f4b26090f1512a2
Kalimaha <guido.barbaglia@gmail.com>
2015-06-02 11:56:03 +0200

    #closes #36 Add destroy() method in the faostat-ui-archetype. Template renamed from .html to .hbs. faostat-ui-commons replaced with FAOSTATCommons WDSClient added.

--- js/faostat-ui-xyz.js
+++ js/faostat-ui-xyz.js
@@ -2,9 +2,11 @@ define(['jquery',
         'handlebars',
         'text!faostat_ui_xyz/html/templates.html',
         'i18n!faostat_ui_xyz/nls/translate',
+        'faostat_commons',
+        'wds_client',
         'bootstrap',
         'sweetAlert',
-        'amplify'], function ($, Handlebars, templates, translate) {
+        'amplify'], function ($, Handlebars, templates, translate, FAOSTATCommons, WDSClient) {
 
     'use strict';
 
@@ -12,9 +14,11 @@ define(['jquery',
 
         this.CONFIG = {
 
+            w: null,
             lang: 'E',
+            prefix: 'faostat_ui_xyz_',
             placeholder_id: 'faostat_ui_xyz',
-            prefix: 'faostat_ui_xyz_'
+            url_wds_crud: 'http://fenixapps2.fao.org/wds_5.1/rest/crud'
 
         };
 
@@ -26,7 +30,20 @@ define(['jquery',
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
 
         /* Fix the language, if needed. */
-        this.CONFIG.lang = this.CONFIG.lang != null ? this.CONFIG.lang : 'E';
+        this.CONFIG.lang = this.CONFIG.lang != null ? this.CONFIG.lang : 'en';
+
+        /* Store FAOSTAT language. */
+        this.CONFIG.lang_faostat = FAOSTATCommons.iso2faostat(this.CONFIG.lang);
+
+        /* Initiate the WDS client. */
+        this.CONFIG.w = new WDSClient({
+            datasource: this.CONFIG.datasource,
+            serviceUrl: this.CONFIG.url_wds_crud
+        });
+
+    };
+
+    MENU.prototype.destroy = function() {
 
     };
 
